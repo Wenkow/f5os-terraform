e82352e9d7db6c2128ca3f9bbe439610c24a88c1
Martin Kylian <m.kylian@c6fk2q6d93.pilsfree.czf>
2024-06-30 21:58:00 +0200

    Add vlans dependencies

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "f5os" {
   host     = var.target1
 }
 
-locals { 
+locals {
   vlans_all = merge(var.vlans_external, var.vlans_internal)
 }
 
@@ -17,25 +17,29 @@ resource "f5os_vlan" "vlans" {
 
 resource "f5os_lag" "TF-LACP-External" {
   name        = "TF-LACP-External"
-  members     = ["3.0","4.0"]
+  members     = ["3.0", "4.0"]
   trunk_vlans = [for vlan in var.vlans_external : vlan.vlanid]
+  depends_on  = [f5os_vlan.vlans]
 }
 
 resource "f5os_lag" "TF-LACP-Internal" {
   name        = "TF-LACP-Internal"
-  members     = ["8.0","9.0"]
+  members     = ["8.0", "9.0"]
   trunk_vlans = [for vlan in var.vlans_internal : vlan.vlanid]
+  depends_on  = [f5os_vlan.vlans]
 }
 
 resource "f5os_tenant" "tf-main" {
   name              = "tf-main"
   image_name        = "BIGIP-17.1.1.3-0.0.5.ALL-F5OS.qcow2.zip.bundle"
-  mgmt_ip           = "10.10.10.11"
-  mgmt_gateway      = "10.10.10.254"
+  mgmt_ip           = "192.168.11.19"
+  mgmt_gateway      = "192.168.11.1"
   mgmt_prefix       = 24
   cpu_cores         = 8
   memory            = 36864
   vlans             = [for vlan in local.vlans_all : vlan.vlanid]
   running_state     = "deployed"
   virtual_disk_size = 82
+  depends_on        = [f5os_lag.TF-LACP-External, f5os_lag.TF-LACP-Internal]
 }
+
